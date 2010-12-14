7306555575c411a11d84f8c40e63daac8e1682f5
mgarcao123 <mgarcao123@gmail.com>
2010-12-14 13:44:40 +0000

    Done with the user edit/update, index and destroy actions 2

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ SampleApp::Application.routes.draw do
   resources :sessions, :only => [:new, :create, :destroy]
   
   match '/signup',  :to => 'users#new'
-  match '/signin',  :to => 'users#new'
-  match '/signout', :to => 'users#destroy'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
