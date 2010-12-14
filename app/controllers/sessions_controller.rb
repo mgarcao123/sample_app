f933fb40c0e8207debd24c1be964848f68e4b9c8
mgarcao123 <mgarcao123@gmail.com>
2010-12-14 13:41:06 +0000

    Done with the user edit/update, index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,11 +14,13 @@ class SessionsController < ApplicationController
     else
       #sign the user in and redirect to the user's show page
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
   end
   
   def destroy
-  end
+      sign_out
+      redirect_to root_path
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
   
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+    
   def show
     @user = User.find(params[:id])
     @title = @user.name
@@ -11,15 +19,51 @@ class UsersController < ApplicationController
   end
   
   def create
-     @user = User.new(params[:user])
-     if @user.save
+    @user = User.new(params[:user])
+    if @user.save
        sign_in @user
        flash[:success] = "Welcome to the Sample App!"
        redirect_to @user
-     else
+    else
        @title = "Sign up"
        render 'new'
-     end
-   end
+    end
+  end
    
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
+  private
+  
+      def authenticate
+        deny_access unless signed_in?
+      end
+      
+      def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_path) unless current_user?(@user)
+      end
+      
+      def admin_user
+        redirect_to(root_path) unless current_user.admin?
+      end
 end
